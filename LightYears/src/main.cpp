ce3e0ffd222eb7cea6e5536e29847ff047729764
Kuntima Kiala <KuntimaKiala>
2024-11-19 02:54:54 +0100

    Real Time Fixed Framerate The Framerate can be selected through the constructor

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-	std::unique_ptr< FromHeLL::Application > App = std::make_unique< FromHeLL::Application >(500, 700, "Light Years 3000");
+	std::unique_ptr< FromHeLL::Application > App = std::make_unique< FromHeLL::Application >(500, 700, "Light Years 3000", 120.0f);
 	App->Run();
 	
 	App.release();
