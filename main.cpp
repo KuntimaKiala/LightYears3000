3fbdb99eaeddc2e678491855f7b2e73a11349fc4
kuntimakiala <kuntima.kiala@gmail.com>
2026-05-23 14:42:59 +0200

    Fix

--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 
 int main()
 {
-<<<<<<< HEAD
 	std::cout << "Entry Point" << std::endl;
 	std::cout << "Entry Point" << std::endl;
 	std::cout << "Entry Point" << std::endl;
-=======
->>>>>>> parent of ff590af (Random Message)
 	std::cout << "" << std::endl;
 }
