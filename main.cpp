d01a958c3514ac5a352650a27cb09cf85d5ebb0d
kuntimakiala <kuntima.kiala@gmail.com>
2026-05-23 14:42:13 +0200

    Revert "Random Message"
    This reverts commit ff590af5aade5b6575b6bfeca10d31e5379cdf30.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
+<<<<<<< HEAD
 	std::cout << "Entry Point" << std::endl;
 	std::cout << "Entry Point" << std::endl;
 	std::cout << "Entry Point" << std::endl;
+=======
+>>>>>>> parent of ff590af (Random Message)
 	std::cout << "" << std::endl;
 }
