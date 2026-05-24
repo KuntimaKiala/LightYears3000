38f70401ec80d872c56f2e6f08a37d05f4bc535d
kuntimakiala <kuntima.kiala@gmail.com>
2026-05-24 02:11:24 +0200

    Setting up SFML and restructuring for the shared libraries

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream> 
-
+#include <SFML/Graphics.hpp>
 
 
 int main() 
 {
+    sf::RenderWindow oRenderWindow(sf::VideoMode({ 100u, 100u }), "Window");
+
     std::cout << "The Obligatory Hello World" << std::endl;
     return 0;
 }
